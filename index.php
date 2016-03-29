1afcf0553ec20666e754816b6cfae2ef7c463bab
Will Boyd <will@codersblock.com>
2016-03-29 09:50:37 -0700

    Make local viewing easier

--- index.php
+++ index.php
@@ -1,25 +1,2 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Diff Cam Project</title>
-
-        <link href="site.css" rel="stylesheet">
-    </head>
-    <body>
-		<h1>Diff Cam Project</h1>
-		<p>
-			Demos and whatever else. Work in progress. Based on <a href="https://github.com/lonekorean/diff-cam-engine">diff-cam-engine</a>. Twitter <a href="https://twitter.com/diffcamproject">@diffcamproject</a>.
-		</p>
-		<ul class="demos">
-			<li>
-				<a href="https://diff-cam-feed.herokuapp.com/">Diff Cam Feed</a>
-				(<a href="https://github.com/lonekorean/diff-cam-feed">diff-cam-feed</a>)
-				<br>
-				Web app that uses motion detection to take a snapshot when something moves, then uploads the image to Twitter.
-			</li>
-		</ul>
-    </body>
-</html>
+// this gets Heroku to use the PHP buildpack for an easy static site
+<?php include_once("home.html"); ?>
